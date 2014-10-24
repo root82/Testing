d92af9c993a3e39be76b2ae487522c8b1e350e05
root82 <muralidhartr@gmail.com>
2014-10-24 12:50:57 +0530

    Testing online

--- LocalTesting1/src/com/murali/test/MainClass.java
+++ LocalTesting1/src/com/murali/test/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 
 		System.out.println("Testing only!!!! -- Small Change -- updating online");
-		System.out.println("Updating again from online!!!");
+		System.out.println("Updating again from online!!!  --- Testing");
 	}
 
 }
