11377ffb8f3348172614df1dad1cdf358c1adca9
root82 <muralidhartr@gmail.com>
2014-10-24 12:20:13 +0530

    First Commit!!!!

---
+++ LocalTesting1/src/com/murali/test/MainClass.java
@@ -0,0 +1,11 @@
+package com.murali.test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testing only!!!!");
+	}
+
+}
